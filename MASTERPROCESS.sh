5170089dab4b1ce6b0d59643f754ef762e944210
Leo McMahon <115442062+LeoMx11@users.noreply.github.com>
2026-04-06 15:28:58 -0700

    Update MASTERPROCESS.sh

--- MASTERPROCESS.sh
+++ MASTERPROCESS.sh
@@ -11,18 +11,18 @@ fi
 
 # Trimmomatic
 
-conda env create -f ~/06-Integration_Project-main/2-Conda_envs/trimmo.yml
+conda env create -f /home/${USER_ID}/06-Integration_Project-main/2-Conda_envs/trimmo.yml
 conda activate trimmo
 echo "Starting trimmomatic at: $(date)"
-python3 ~/06-Integration_Project-main/3-TRIM/MASTERPROCESS_trimmomatic.py ${USER_ID}
+python3 /home/${USER_ID}/06-Integration_Project-main/3-TRIM/MASTERPROCESS_trimmomatic.py ${USER_ID}
 echo "Finished trimmomatic at: $(date)"
 conda deactivate
 
 # Create reference for rsem/star
 
-conda env create -f ~/06-Integration_Project-main/2-Conda_envs/rsem_env.yml
+conda env create -f /home/${USER_ID}/06-Integration_Project-main/2-Conda_envs/rsem_env.yml
 conda activate rsem_env
-cd ~/06-Integration_Project-main/4-MAP
+cd /home/${USER_ID}/06-Integration_Project-main/4-MAP
 rm -rf reference
 mkdir reference
 echo "Reference created at: $(date)"
